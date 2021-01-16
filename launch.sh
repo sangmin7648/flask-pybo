48a03877f68ef4e1b84089575b42ca48314bda8f
Sangmin Lee <sangminlee7648@gmail.com>
2021-01-16 16:49:19 +0900

    launch.sh 변경

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export FLASK_APP=/home/sangmin/PycharmProjects/flask_project/pybo/__init__.py
+export FLASK_APP=pybo
 export FLASK_ENV=development
+export APP_CONFIG_FILE=/home/sangmin/PycharmProjects/flask_project/config/development.py
 flask run
